cfa316033de23a25e3b8805de8246dfa1a9b3e0a
Spencer Freebairn <spencer.freebairn@gmail.com>
2019-09-24 19:46:28 -0700

    move script to external file, edit class name for dark shadows

---
+++ javascript/resize_youtube_videos.js
@@ -0,0 +1,13 @@
+// Resizes all embedded YouTube videos if browser is too narrow
+let videos = document.querySelectorAll("iframe.youtube-video");
+
+for (let i = 0; i < videos.length; i++) {
+   let originalWidth =  videos[i].width;
+   let originalHeight = videos[i].height;
+
+   if (videos[i].width > window.innerWidth) {
+      videos[i].width = window.innerWidth;
+      videos[i].height = window.innerWidth * originalHeight / originalWidth;
+   }
+}
+
