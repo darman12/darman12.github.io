d0b686274357d79df3d6a31f4e69c4045649d7d2
Spencer Freebairn <spencer.freebairn@gmail.com>
2019-09-28 17:49:49 -0700

    refactored YouTube resize script

--- javascript/resize_youtube_videos.js
+++ javascript/resize_youtube_videos.js
@@ -2,12 +2,15 @@
 let videos = document.querySelectorAll("iframe.youtube-video");
 
 for (let i = 0; i < videos.length; i++) {
-   let originalWidth =  videos[i].width;
+   let originalWidth = videos[i].width;
    let originalHeight = videos[i].height;
 
    if (videos[i].width > window.innerWidth) {
       videos[i].width = window.innerWidth;
-      videos[i].height = window.innerWidth * originalHeight / originalWidth;
+      videos[i].height = scaleRectangle(originalHeight, originalWidth,window.innerWidth); 
    }
 }
 
+function scaleRectangle(originalX, originalZ, newX) {
+   return newX * originalX / originalZ;
+}
